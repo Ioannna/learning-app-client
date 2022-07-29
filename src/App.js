c1bbc64fc913a07dc3d52562cc31df39a85d256a
Ioannna <ioanabiiris@gmail.com>
2022-07-29 16:12:15 +0300

    update client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [connectedSocket, setConnectedSocket] = useState()
+
+  useEffect (() => {
+    const socket = io()
+
+    socket.on("connected", () => {
+      setConnectedSocket(socket)
+    })
+  }, [])
+
+  if (!connectedSocket) {
+    return <h1>Waiting for connection...</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
   );
 }
