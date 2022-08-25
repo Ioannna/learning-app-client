64ce2d2b662c50c488cf5aac01d8a99e0a848cbb
Ioannna <ioanabiiris@gmail.com>
2022-08-25 10:37:16 +0300

    update3 trysheet.js

--- src/components/lesson/trysheet.js
+++ src/components/lesson/trysheet.js
@@ -4,6 +4,7 @@ const TrySheet = () => {
             <h1>This is a try sheet! </h1>
             <p>Now I've added another modification! </p>
             <p>git push -u origin master</p>
+            <p>git push </p>
         </div>
     )
 }
